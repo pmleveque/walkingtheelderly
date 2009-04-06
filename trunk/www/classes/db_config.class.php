84ef25a017e1a6cdd6945dcb4bd8c9ef6b67cb30
pmleveque@gmail.com <pmleveque@gmail.com@7f58e8c4-1eea-11de-896d-b39e94f1936c>
2009-04-06 19:43:03 +0000

    Novo

---
+++ trunk/www/classes/db_config.class.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Description of user
+ *
+ * @author pmleveque
+ */
+
+
+class db {
+
+
+     /*Settings*/
+  /**
+   * The database that we will use
+   * var string
+   */
+  var $dbName = 'wte';
+  /**
+   * The database host
+   * var string
+   */
+  var $dbHost = 'localhost';
+  /**
+   * The database port
+   * var int
+   */
+  var $dbPort = 3306;
+  /**
+   * The database user
+   * var string
+   */
+  var $dbUser = 'root';
+  /**
+   * The database password
+   * var string
+   */
+  var $dbPass = 'certus';
+  /**
+   * The database table that holds all the information
+   * var string
+   */
+  
+}
+?>
