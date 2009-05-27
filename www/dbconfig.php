0f2295da4ef7df1eb332e710fc66a3fd629672b6
pmleveque <pmleveque@7f58e8c4-1eea-11de-896d-b39e94f1936c>
2009-05-27 12:43:43 +0000

    db

--- www/dbconfig.php
+++ www/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 
 //Connection to the database
-$link = mysql_connect('http://143.107.102.3:8080', 'root', 'comp2010') or die(mysql_error());
+$link = mysql_connect('143.107.102.3', 'root', 'comp2010') or die(mysql_error());
 $bdName = "grupo3";
 
 
