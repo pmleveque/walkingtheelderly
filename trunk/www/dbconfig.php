5a80eb6440ed810da1c3f48eaddd85cac7d24450
pmleveque <pmleveque@7f58e8c4-1eea-11de-896d-b39e94f1936c>
2009-05-27 13:43:58 +0000

    data base funcionando !!!!!!!

--- trunk/www/dbconfig.php
+++ trunk/www/dbconfig.php
@@ -1,8 +1,9 @@
 <?php
 
 //Connection to the database
-$link = mysql_connect('localhost', 'root', 'certus') or die(mysql_error());
-$bdName = "wte";
+//$link = mysql_connect('localhost', 'root', 'certus') or die(mysql_error());
+$link = mysql_connect('143.107.102.3:3306', 'walking', 'comp2010') or die(mysql_error());
+$bdName = "grupo3";
 
 
 ?>
